bcd905e7a6ef47107a2979d86d0acabed0346561
adriel <adriel.martinelli@gmail.com>
2024-05-29 09:07:13 -0300

    criando repositorio

---
+++ farmacia/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    colors:{
+      
+      'azulEscuro':'#335e90',
+      'azulClaro':'#87c6ec',
+      'verde' :'#5eba24',
+      'verdeClaro':'#c6e7c9',
+      'cinza':'#E0E1DD',
+      'preto':'#000000',
+      'branco':'#ffffff',
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
